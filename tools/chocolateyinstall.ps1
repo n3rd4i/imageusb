62ed097391fcc0b6e31016571d3500b34fd6d4a8
Andrei Hutanu <andrei.hutanu@evalan.com>
2019-12-16 17:16:17 +0100

    initial version

---
+++ tools/chocolateyinstall.ps1
@@ -0,0 +1,11 @@
+﻿$ErrorActionPreference = 'Stop';
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url        = 'https://www.osforensics.com/downloads/imageusb.v1.4.1003.zip'
+$packageArgs = @{
+  packageName   = $env:ChocolateyPackageName
+  unzipLocation = $toolsDir
+  url           = $url
+  checksum      = 'EE966CFAF59006FDB6CB8B1ECF4A0BC4D3FF5701820AE09D5D436109E927DFCB'
+  checksumType  = 'sha256'
+}
+Install-ChocolateyZipPackage @packageArgs
